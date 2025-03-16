4f5f452411a54bd460f681816408823cf3f785ba
LuckyKing0307 <jasaergasheva@gmail.com>
2025-03-16 21:18:41 +0500

    first

--- post_to_sheets.php
+++ post_to_sheets.php
@@ -8,14 +8,4 @@ use Google\Client;
 define('SERVICE_ACCOUNT_JSON', 'keys.json');
 
 $file = json_decode(file_get_contents('keys.json'),1);
-var_dump(file_get_contents('keys.json'));
-$client = new Client();
-
-try {
-    $client->setAuthConfig(SERVICE_ACCOUNT_JSON);
-    $client->setScopes(['https://www.googleapis.com/auth/spreadsheets.readonly']);
-    $client->fetchAccessTokenWithAssertion();
-    echo "✅ Авторизация прошла успешно!";
-} catch (Exception $e) {
-    echo "❌ Ошибка: " . $e->getMessage();
-}
+var_dump(json_decode(file_get_contents('keys.json'),1));
