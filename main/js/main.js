73a2af7117390ce9485b10a1215640d64f0eb89b
LuckyKing0307 <jasaergasheva@gmail.com>
2025-03-16 22:05:42 +0500

    first

--- main/js/main.js
+++ main/js/main.js
@@ -96,6 +96,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('phone').addEventListener('input', function (e) {
         let x = e.target.value.replace(/\D/g, '').match(/(\d{3})(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
         e.target.value = "+998" + (x[2] ? ` (${x[2]}` : '') + (x[3] ? `) ${x[3]}` : '') + (x[4] ? `-${x[4]}` : '') + (x[5] ? `-${x[5]}` : '');
+        e.target.style.color = '#000';
     });
     function startTimer() {
         const timerInterval = setInterval(() => {
