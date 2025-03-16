9d6461b609ad2572ff9960772864b4c5aab2c636
LuckyKing0307 <jasaergasheva@gmail.com>
2025-03-16 20:27:28 +0500

    first

--- post_to_sheets.php
+++ post_to_sheets.php
@@ -5,7 +5,8 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require 'vendor/autoload.php';
 use Google\Client;
-
+$file = file_get_contents('keys.json');
+var_dump($file);
 define('SERVICE_ACCOUNT_JSON', 'keys.json');
 
 $client = new Client();
